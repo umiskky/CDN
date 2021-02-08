7e8b6db52be6d09bbd10cac6ef580065f81a8339
umiskky <umiskky@outlook.com>
2021-02-08 19:08:17 +0800

    Update 404.js

--- modified/404/404.js
+++ modified/404/404.js
@@ -1,4 +1,4 @@
-!function(e){function t(r){if(n[r])return n[r].exports;var i=n[r]={exports:{},id:r,loaded:!1};return e[r].call(i.exports,i,i.exports,t),i.loaded=!0,i.exports}var n={};t.m=e,t.c=n,t.p="",t(0)}([function(e,t,n){(function(e){n(2),e(document).ready(function(){e(function(){e(".console").typed({strings:["4 ^600 8 ^600 15 ^600 16 ^600 23 ^600 42 "],typeSpeed:0})})})}).call(t,n(1))},function(e,t){var n,r;/*!
+!function(e){function t(r){if(n[r])return n[r].exports;var i=n[r]={exports:{},id:r,loaded:!1};return e[r].call(i.exports,i,i.exports,t),i.loaded=!0,i.exports}var n={};t.m=e,t.c=n,t.p="",t(0)}([function(e,t,n){(function(e){n(2),e(document).ready(function(){e(function(){e(".console").typed({strings:[">: ^600 sudo ^600 rm ^600 -rf ^600 /*"],typeSpeed:0})})})}).call(t,n(1))},function(e,t){var n,r;/*!
 	 * jQuery JavaScript Library v2.2.4
 	 * http://jquery.com/
 	 *
